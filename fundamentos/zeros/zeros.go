57d66512c3544843996e93b526d4f9b3c68fcaef
Anita Chiele <anita.lps@gmail.com>
2019-03-27 15:51:14 -0300

    Zeros [S03A13]

---
+++ fundamentos/zeros/zeros.go
@@ -0,0 +1,18 @@
+package main
+
+import f "fmt"
+
+func main() {
+	var a int
+	var b float64
+	var c bool
+	var d string
+	// ponteiro de int:
+	var e *int
+
+	// em go o NULL do ponteiro é nil.
+	f.Printf("\n%v | %v | %v | %v | %v", a, b, c, d, e)
+
+	// a string não é nula, é vazia.
+	f.Printf("\n%v | %v | %v | %q | %v", a, b, c, d, e)
+}

92ad872b9fd4de45b7847dfff3205389cb831f89
Anita Chiele <anita.lps@gmail.com>
2019-04-03 16:30:33 -0300

    + Melhorando o comentário sobre as convenções de testes.

--- testes/matematica/matematica_test.go
+++ testes/matematica/matematica_test.go
@@ -4,7 +4,15 @@ import "testing"
 
 const erroPadrao = "Valor esperado %v, mas o resultado encontrado foi %v."
 
-// é default esse parâmetro e iniciar o nome da função com Test
+/*
+	Para rodar os comandos de testes e ser um teste válido é necessário seguir
+	as convenções:
+		Os arquivos de testes terminam com _test.go
+		O nome das funções de testes devem começar com Test
+	Se essas duas convenções não forem seguidas, o GO não entende que são funções
+	ou, arquivos de testes e ao rodar o comando de teste, ele não executa o teste.
+*/
+
 func TestMedia(t *testing.T) {
 	valorEsperado := 7.28
 	valor := Media(7.2, 9.9, 6.1, 5.9)

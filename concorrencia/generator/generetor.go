b4536f1c34ad1fc7cf9959b88aa653a711ffada1
Anita Chiele <anita.lps@gmail.com>
2019-04-03 14:46:54 -0300

    [S09A74] Aleterado para implementar a utilização da função através da lib.

--- concorrencia/generator/generetor.go
+++ concorrencia/generator/generetor.go
@@ -2,9 +2,8 @@ package main
 
 import (
 	f "fmt"
-	"io/ioutil"
-	"net/http"
-	"regexp"
+
+	"github.com/anitachiele/go_src/titulo"
 )
 
 /*
@@ -12,31 +11,9 @@ import (
 	https://www.youtube.com/watch?v=f6kdp27TYZs
 */
 
-/*
-	Essa função é do tipo generator porque retorna um channel somente-leitura.
-	Ou seja, a função que irá chamar essa função não precisará se preocupar em
-	criar o channel nem go routines.
-*/
-func titulo(urls ...string) <-chan string {
-	c := make(chan string)
-
-	for _, url := range urls {
-		// função anônima
-		go func(url string) {
-			resp, _ := http.Get(url)
-			html, _ := ioutil.ReadAll(resp.Body)
-
-			r, _ := regexp.Compile("<title[^>]*>([^<]+)</title>")
-			c <- r.FindStringSubmatch(string(html))[1]
-		}(url)
-	}
-
-	return c
-}
-
 func main() {
-	t1 := titulo("https://www.cod3r.com.br", "https://www.google.com")
-	t2 := titulo("https://www.amazon.com", "https://www.youtube.com")
+	t1 := titulo.GetTitulo("https://www.cod3r.com.br", "https://www.google.com")
+	t2 := titulo.GetTitulo("https://www.amazon.com", "https://www.youtube.com")
 
 	f.Println("Primeiros: ", <-t1, "|", <-t2)
 	f.Println("Segundos: ", <-t1, "|", <-t2)
